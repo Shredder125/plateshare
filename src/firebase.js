8edbe87e076692edcb3f3da8219d3599fe5d6814
Shredder125 <rabighoshamartya@gmail.com>
2025-11-12 22:47:53 +0530

    secured firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,16 @@
-// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDpv8X4acXEZSihbk5LHuEGlB3NNZz0tCw",
-  authDomain: "plateshare-8f801.firebaseapp.com",
-  projectId: "plateshare-8f801",
-  storageBucket: "plateshare-8f801.firebasestorage.app",
-  messagingSenderId: "862254191872",
-  appId: "1:862254191872:web:4f788a503fd5c0d62acfe5"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 
-// Firebase Auth
 export const auth = getAuth(app);
-
-// Google Provider for Google Sign-In
 export const provider = new GoogleAuthProvider();
